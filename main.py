69c35ebb13000b9a36325b29298dd87a8c74531d
Mukhtar Rahimi <mukhtarrahimi110@gmail.com>
2025-08-24 21:51:33 +0430

    add variable

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
 import sys
+
+LENGTH_FACTORS = {
+    "m": 1.0,
+    "km": 1000.0,
+    "cm": 0.01,
+    "mm": 0.001,
+    "mi": 1609.344,  # mile
+    "yd": 0.9144,  # yard
+    "ft": 0.3048,  # foot
+    "in": 0.0254,  # inch
+}
